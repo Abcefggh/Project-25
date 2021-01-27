b026f3b803d9fb650b2a6be9998bab1ece49593c
Abcefggh <75517235+Abcefggh@users.noreply.github.com>
2021-01-27 18:29:41 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,72 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+var dustbinObj,groundObject	
+var world;
+var paper;
+var dustbinImage
+var paperImage
+
+function preload() {
+	 
+	paperImage = loadImage("paper.png");
+	dustbinImage = loadImage("dustbingreen.png");
+}
+
+
+
+function setup() {
+	createCanvas(1600, 700);
+	rectMode(CENTER);
+
+
+	engine = Engine.create();
+	world = engine.world;
+	
+	groundObject=new ground(width/2,670,width,20);
+	dustbinObj=new dustbin(1200,650);
+	 paper =new Paper(100,650,20,20); 
+	 
+	
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(230);
+  Engine.update(engine);
+
+  groundObject.display();
+  dustbinObj.display();
+  paper.display();
+  
+
+  image(dustbinImage,1090,440,210,210);
+ 
+
+keyPressed();
+
+}
+
+function keyPressed() {
+	if (keyDown(UP_ARROW))
+	 {
+
+	  Matter.Body.applyForce(paper.body,paper.body.position,{x:150,y:-150});
+
+  
+	}
+}
+
+
+
+
+  
+  
+  
+
